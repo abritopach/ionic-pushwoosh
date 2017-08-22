665f7832ab061cf69b6bf1a51ad7df97215d758f
Adrián Brito <adrbrpa1988@gmail.com>
2017-08-22 10:57:02 +0200

    Updated

--- src/providers/pushwoosh-service/pushwoosh-service.ts
+++ src/providers/pushwoosh-service/pushwoosh-service.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 
 import { Platform } from 'ionic-angular';
 
-//import { Device } from '@ionic-native/device';
 declare var cordova : any;
 
 // For iOS, open your project .pList file in xCode and add:
@@ -14,14 +13,11 @@ declare var cordova : any;
 export class PushwooshService {
 
     PUSHWOOSH_APP_ID : string = 'XXXXX-XXXXX'; // Your pushwoosh app id.
-    GOOGLE_PROJECT_NUMBER : string = 'XXXXXXXXXXXXX'; // Project number from firebase.
+    GOOGLE_PROJECT_NUMBER : string = 'XXXXXXXXXXXX'; // Project number from firebase.
 
     constructor(public platform : Platform){
 
-        console.log("Constructor");
-
         this.platform.ready().then(() => {
-            console.log("Platform ready");
             if (this.platform.is('ios')) {
                 console.log("PushwooshService init: Running on push compatible platform "+ this.platform.userAgent() +')');
                 this.initIOS();
@@ -40,52 +36,28 @@ export class PushwooshService {
 
     }
 
-    /*
-    init() {
-        console.log("PushwooshService init");
-
-        if(!Device.device) {
-            console.log("PushwooshService init: No device object available.  Skipping init.  (Probably not running in a deployed Cordova app.)");
-            return;
-        }
-
-        switch (Device.device.platform) {
-            case 'iOS':
-                console.log('Starting iOS Pushwoosh initialization');
-                this.initIOS();
-                break;
-            case 'Android':
-                console.log('Starting Android Pushwoosh initialization');
-                this.initAndroid();
-                break;
-            default:
-                console.log('Unknown Cordova platform', Device.device.platform, '. Skipping Pushwoosh initialization');
-        }
-    }
-    */
-
     initIOS() {
         let pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
-        //set push notification callback before we initialize the plugin
+        // Set push notification callback before we initialize the plugin.
         document.addEventListener('push-notification', function (event:any) {
-            //get the notification payload
+            // Get the notification payload.
             let notification = event.notification;
 
-            //display alert to the user for example
+            // Display alert to the user for example.
             alert(notification.aps.alert);
 
-            //clear the app badge
+            // Clear the app badge.
             pushNotification.setApplicationIconBadgeNumber(0);
         });
 
-        //initialize the plugin
+        // Initialize the plugin.
         pushNotification.onDeviceReady({pw_appid: this.PUSHWOOSH_APP_ID});
 
-        //register for pushes
+        // Register for pushes.
         pushNotification.registerDevice(
             function (status) {
-                var deviceToken = status['deviceToken'];
+                let deviceToken = status['deviceToken'];
                 console.warn('registerDevice: ' + deviceToken);
             },
             function (status) {
@@ -94,23 +66,24 @@ export class PushwooshService {
             }
         );
 
-        //reset badges on app start
+        // Reset badges on app start.
         pushNotification.setApplicationIconBadgeNumber(0);
     }
 
     initAndroid() {
         let pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
-        //set push notifications handler
+        // Set push notifications handler.
         document.addEventListener('push-notification', function (event:any) {
             let title    = event.notification.title;
+            let message = event.notification.message;
             let userData = event.notification.userdata;
 
             if (typeof(userData) != "undefined") {
                 console.warn('user data: ' + JSON.stringify(userData));
             }
 
-            alert(title);
+            alert(message);
         });
 
         // Initialize Pushwoosh with projectid: GOOGLE_PROJECT_NUMBER, pw_appid : PUSHWOOSH_APP_ID. This will trigger all
@@ -124,7 +97,7 @@ export class PushwooshService {
         // Register for pushes.
         pushNotification.registerDevice(
             function (status) {
-                var pushToken = status;
+                let pushToken = status;
                 console.warn('push token: ' + pushToken);
                 alert('push token: ' + JSON.stringify(pushToken));
             },
